03e6c2c01eeed3436ec09ce83cb518dc096a7366
kn1cht <ito.turukasiwa@gmail.com>
2017-06-27 16:36:00 +0900

    unit test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,52 @@
-const assert = require('chai').assert;
+const assert = require('power-assert');
 const nock = require('nock');
 const mockery = require('mockery');
 const rewire = require('rewire');
+
 const main = require('../main.js');
+const main_rewire = rewire('../main.js');
 
 const fake_server = 'fake.server.com';
 const fake_username = 'user';
 const fake_password = 'pass';
+
+describe('katakanaToHiragana', () => {
+  it('convert カタカナ to ひらがな', () => {
+    const katakanaToHiragana = main_rewire.__get__('katakanaToHiragana');
+    assert(katakanaToHiragana('アットマーク') === 'あっとまーく');
+  });
+  it('do nothing against not カタカナ character', () => {
+    const katakanaToHiragana = main_rewire.__get__('katakanaToHiragana');
+    assert(katakanaToHiragana('あAa1!') === 'あAa1!');
+  });
+});
+
+describe('normalizeForDefaultSort', () => {
+  it('convert 濁音 to 清音', () => {
+    const normalizeForDefaultSort = main_rewire.__get__('normalizeForDefaultSort');
+    assert(normalizeForDefaultSort('ゔがぎぐげござじずぜぞだぢづでどばびぶべぼ') === 'うかきくけこさしすせそたちつてとはひふへほ');
+  });
+  it('convert 半濁音 to 清音', () => {
+    const normalizeForDefaultSort = main_rewire.__get__('normalizeForDefaultSort');
+    assert(normalizeForDefaultSort('ぱぴぷぺぽ') === 'はひふへほ');
+  });
+  it('convert 拗音 to 直音', () => {
+    const normalizeForDefaultSort = main_rewire.__get__('normalizeForDefaultSort');
+    assert(normalizeForDefaultSort('きゃきゅきょ') === 'きやきゆきよ');
+  });
+  it('convert 促音 to 直音', () => {
+    const normalizeForDefaultSort = main_rewire.__get__('normalizeForDefaultSort');
+    assert(normalizeForDefaultSort('きっとかっと') === 'きつとかつと');
+  });
+  it('convert 長音 to 母音', () => {
+    const normalizeForDefaultSort = main_rewire.__get__('normalizeForDefaultSort');
+    assert(normalizeForDefaultSort('あーきーすーつーのー') === 'ああきいすうつうのお');
+  });
+  it('do nothing against not ひらがな character', () => {
+    const normalizeForDefaultSort = main_rewire.__get__('normalizeForDefaultSort');
+    assert(normalizeForDefaultSort('Abc123"#$') === 'Abc123"#$');
+  });
+});
 
 describe('defaultSortBot', () => {
   before(() => {
@@ -29,15 +69,5 @@ describe('defaultSortBot', () => {
     //nock.cleanAll();
     //return mockery.disable();
   });
-  //this.timeout(10000);
-
-  describe('katakana to hiragana converter', () => {
-    it('should return あっとまーくAa01 when the value is アットマークAa01', () => {
-        const main_rewire = rewire('../main.js');
-        const katakanaToHiragana = main_rewire.__get__('katakanaToHiragana');
-        assert.strictEqual(katakanaToHiragana('アットマークAa01'), 'あっとまーくAa01');
-      });
-    });
-
-
+  it('get mediawiki pages and add proper sort key');
 })
